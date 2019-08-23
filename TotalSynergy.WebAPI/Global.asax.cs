b6ef735b3030fbf1d4238f3d38bac1e4d17916db
manik.saha <manik.saha@elmolearning.com.au>
2019-08-23 23:59:08 +1000

    Dynamic DB path

--- TotalSynergy.WebAPI/Global.asax.cs
+++ TotalSynergy.WebAPI/Global.asax.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
 using System.Web.Mvc;
-
+using System.Web.Hosting;
 
 namespace TotalSynergy.WebAPI
 {
@@ -13,6 +13,9 @@ namespace TotalSynergy.WebAPI
     {
         protected void Application_Start()
         {
+            //AppDomain.CurrentDomain.SetData("DataDirectory", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            string path = HostingEnvironment.MapPath(@"~/App_Data");
+            AppDomain.CurrentDomain.SetData("DataDirectory", path);
             GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
